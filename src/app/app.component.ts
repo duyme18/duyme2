a68ff15a8fb354ff74e789ce2ada5a0ff8242f47
Duy <duyme18@gmail.com>
2020-12-25 09:58:15 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'duyme2-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'duyme2';
+}
