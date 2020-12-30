7512f1959faa0e7ef7b534ed9f8dc026ee8031d1
Duy <duyme18@gmail.com>
2020-12-30 17:32:31 +0700

    validate author name

--- src/app/author-form/author-form.component.ts
+++ src/app/author-form/author-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { throwIfEmpty } from 'rxjs/operators';
 import { Author } from '../models/author';
 import { AuthorService } from '../_services/author.service';
 import { CommonService } from '../_services/common.service';
