fd7affd908cc9abe828b8d03d5613db736b1b48a
Duy <duyme18@gmail.com>
2021-01-06 17:00:32 +0700

    upload file

--- src/app/_services/upload-file.service.ts
+++ src/app/_services/upload-file.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UploadFileService {
 
-  constructor() { }
+  private readonly homeUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  upload(file: File | undefined): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+
+    // @ts-ignore
+    formData.append('file', file);
+
+    const req = new HttpRequest('POST', `${this.homeUrl}upload`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+
+    return this.http.request(req);
+  }
+
+  getFiles(): Observable<any> {
+    return this.http.get(`${this.homeUrl}files`);
+  }
 }
