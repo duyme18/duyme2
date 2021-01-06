2a3d788daf8855be07e31552e8c7079cb1912aa4
Duy <duyme18@gmail.com>
2021-01-06 16:55:00 +0700

    commit

---
+++ src/app/upload-files/upload-files.component.ts
@@ -0,0 +1,53 @@
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UploadFileService } from '../_services/upload-file.service';
+
+@Component({
+  selector: 'duyme2-upload-files',
+  templateUrl: './upload-files.component.html',
+  styleUrls: ['./upload-files.component.scss']
+})
+export class UploadFilesComponent implements OnInit {
+
+  selectedFiles?: FileList;
+  currentFile?: File;
+  progress = 0;
+  message = '';
+
+  fileInfos?: Observable<any>;
+
+  constructor(private uploadService: UploadFileService) { }
+  ngOnInit(): void {
+    this.fileInfos = this.uploadService.getFiles();
+  }
+
+  // tslint:disable-next-line:typedef
+  selectFile(event: any) {
+    this.selectedFiles = event.target.files;
+  }
+
+  upload() {
+    this.progress = 0;
+
+    // @ts-ignore
+    this.currentFile = this.selectedFiles.item(0);
+    this.uploadService.upload(this.currentFile).subscribe(
+      event => {
+        if (event.type === HttpEventType.UploadProgress) {
+          // @ts-ignore
+          this.progress = Math.round(100 * event.loaded / event.total);
+        } else if (event instanceof HttpResponse) {
+          this.message = event.body.message;
+          this.fileInfos = this.uploadService.getFiles();
+        }
+      },
+      err => {
+        this.progress = 0;
+        this.message = 'Could not upload the file!';
+        this.currentFile = undefined;
+      });
+
+    this.selectedFiles = undefined;
+  }
+}
